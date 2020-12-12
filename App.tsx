7725d8a03da00a6944201ee2d3b0c65582d36de8
KrJrr <jungu8787@naver.com>
2020-12-12 18:16:59 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView,  StyleSheet, StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+
+const App: () => React.Node = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default App;
